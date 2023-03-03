8a3c12350ab8902f521a7db602ba543b418eee79
Alleni Sai Siddartha <asaisidd.visteon.com>
2023-03-03 01:04:30 -0800

    clang-tidy const on LHS check test

---
+++ hello.c
@@ -0,0 +1,14 @@
+#define HELLO 4
+
+int main(void){
+	int l_param = 0;
+	int l_h = 0;
+	if(l_param==HELLO){
+	}
+	else if(HELLO==l_param){
+
+	}
+	else if(l_param<=l_h){
+	}
+	return 0;
+}
---
+++ url.c
@@ -0,0 +1,19 @@
+#include<stdio.j>
+
+
+struct status {
+	char param[10];
+	int stat;
+}
+
+struct url {
+	char url[300];
+	struct status[3];
+}
+
+struct url getUrl(char* s){
+
+
+int main(void) {
+	char s[300];
+
